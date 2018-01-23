29bb0d288e54b980c4274deedb5169289bc7e740
mhunter125 <32371663+mhunter125@users.noreply.github.com>
2018-01-23 16:53:42 -0500

    Interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
-import './album';
 import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
